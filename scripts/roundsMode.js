c3960e70dbb039a651c8bbc64d8b15a3abbb181d
Gerald Hendrix <geraldhendrix@Geralds-MacBook-Pro.local>
2026-04-28 23:05:08 -0700

    Initial creation of roundsMode.js #5

---
+++ scripts/roundsMode.js
@@ -0,0 +1,20 @@
+/*************************************************************************
+ * File: roundsMode.js
+ * This file contains functions that support the "Rounds" mode, 
+ * including the "Log Round" modal.
+*************************************************************************/
+
+/*************************************************************************
+ * @function updateSGS 
+ * @Desc 
+ * When the strokes, minutes or seconds fields are updated, 
+ * update the speedgolf score accordingly.
+ * @global roundStrokes: Form's strokes field
+ * @global roundMinutes: Form's minutes field
+ * @global roundSeconds: Form's seconds field
+ *************************************************************************/
+ function updateSGS() {
+  GlobalRoundSGS.value = 
+    (GlobalRoundStrokes.valueAsNumber + GlobalRoundMinutes.valueAsNumber) + 
+    ":" + GlobalRoundSeconds.value
+}
