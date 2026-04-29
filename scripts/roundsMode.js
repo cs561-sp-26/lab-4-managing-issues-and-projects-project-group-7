311b30a7d41cb77ab940e5c16e7656de1093f569
Gerald Hendrix <geraldhendrix@Geralds-MacBook-Pro.local>
2026-04-28 23:10:29 -0700

    Added Search and Log Functions to roundsMode.js #5

--- scripts/roundsMode.js
+++ scripts/roundsMode.js
@@ -410,3 +410,210 @@ for (let i = 1; i <=3; ++i) {
   }
 }
 }
+
+/*************************************************************************
+* @function GlobalRoundsTableSortBtns CLICK Handler 
+* @desc 
+* When one of the sort buttons in a sortable Rounds Table header column
+* is clicked, sort by that column using sortRoundsTable function
+* @global GlobalRoundsTableSortBtns: Array of buttons in col header that
+*         can be clicked 
+*************************************************************************/
+for (let i = 0; i < GlobalRoundsTableSortBtns.length; ++i) {
+GlobalRoundsTableSortBtns[i].addEventListener("click",() => sortRoundsTable(i+1) );
+}
+
+/*************************************************************************
+* @function searchRoundsTable 
+* @Desc 
+* When the user performs a keystroke within the search box, perform a 
+* search of the rounds table, displaying only those rows that contain
+* the text within the search box.
+* @param searchVal, the text string in the search box
+* @global createAccountForm: the <form> element whose 
+*         SUBMIT handler is triggered
+* @global roundsTable: The table of rounds
+*************************************************************************/
+function searchRoundsTable(searchVal) {
+searchVal = searchVal.toUpperCase(); //case insensitive
+let tr = GlobalRoundsTable.getElementsByTagName("tr");
+let td, rowText, i, j;
+let numVisibleRows = 0;
+for (i = 1; i < tr.length; i++) {  //Loop through all table rows
+  td = tr[i].getElementsByTagName("td");
+  rowText = "";
+  for (j = 0; j < 3; ++j) { //only consider Date, Course, Score cols
+    rowText += td[j].textContent;
+  }
+  if (rowText != "") {
+    if (rowText.toUpperCase().indexOf(searchVal) > -1) {
+      tr[i].style.display = ""; //show row
+      numVisibleRows++;
+    } else {
+      tr[i].style.display = "none"; //hide row
+    }
+  }
+}
+if (numVisibleRows == 1) {
+  GlobalRoundsTableCaption.textContent = "Table displaying 1 speedgolf round";
+} else {
+  GlobalRoundsTableCaption.textContent = "Table displaying " + numVisibleRows + " speedgolf rounds";
+}
+}
+
+/*************************************************************************
+* @function searchBtn CLICK handler 
+* @Desc 
+* If the user clicks the search button when the search box is invisible,
+* show the search box and set the focus on it. If the user clicks the 
+* search button when the search box is visible, hide the search box and
+* set the focus on the search button. 
+* @global searchBox: The search box
+* @global searchBtn: The search button
+*************************************************************************/
+GlobalSearchBtn.addEventListener("click",function() {
+if (GlobalCurrentMode.get() === 1) {
+    if (GlobalSearchBox.classList.contains("hidden")) {
+        GlobalSearchBtn.setAttribute("aria-label","Close rounds search");
+        GlobalSearchBox.classList.remove("hidden");
+        GlobalSearchBox.focus(); 
+    } else {
+        GlobalSearchBox.value = "";
+        searchRoundsTable(GlobalSearchBox.value);
+        GlobalSearchBox.classList.add("hidden");
+        GlobalSearchBtn.setAttribute("aria-label","Open rounds search");
+        GlobalSearchBtn.focus();
+    }
+}
+});
+
+/*************************************************************************
+* @function logRoundForm SUBMIT Handler 
+* @Desc 
+* When the user clicks on the "Add Round" button, we first check the
+* validity of the fields, presenting accessible
+* error notifications if errors exist. If no errors exist, we
+* call the logRound() function, passing in the round data
+* @global createAccountForm: the <form> element whose 
+*         SUBMIT handler is triggered
+* @global GlobalRoundDate: the field containing the round date
+* @global GlobalRoundCourse: the course of the round
+* @global GlobalRoundStrokes: the number of strokes taken in the round
+* @global GlobalRoundMinutes: the number of minutes taken in the round
+* @global GlobalRoundSeconds: the number of seconds taken in teh round
+* @global GlobalRoundNotes: the notes on the round
+*************************************************************************/
+GlobalLogRoundForm.addEventListener("submit",function(e) {
+e.preventDefault(); //Prevent default submit behavior
+//Is the date valid?
+let dateValid = !GlobalRoundDate.validity.valueMissing;
+//Is the course field valid?
+let courseValid = !GlobalRoundCourse.validity.tooLong && 
+                  !GlobalRoundCourse.validity.valueMissing;
+//Is the password field valid?
+let strokesValid = !GlobalRoundStrokes.validity.typeMismatch &&
+                   !GlobalRoundStrokes.validity.rangeUnderflow &&
+                   !GlobalRoundStrokes.validity.rangeOverflow && 
+                   !GlobalRoundStrokes.validity.valueMissing;
+//Is the minutes field valid?
+let minutesValid = !GlobalRoundMinutes.validity.typeMismatch &&
+                   !GlobalRoundMinutes.validity.rangeUnderflow &&
+                   !GlobalRoundMinutes.validity.rangeOverflow && 
+                   !GlobalRoundMinutes.validity.valueMissing;
+//Is the seconds field valid?
+let secondsValid = !GlobalRoundSeconds.validity.typeMismatch &&
+                   !GlobalRoundSeconds.validity.rangeUnderflow &&
+                   !GlobalRoundSeconds.validity.rangeOverflow && 
+                   !GlobalRoundSeconds.validity.valueMissing;
+//Is the notes field valid?
+let notesValid = !GlobalRoundNotes.validity.tooLong;
+if (courseValid && strokesValid && minutesValid &&
+    secondsValid && notesValid &&dateValid) { 
+    //All is well -- log round or update round
+    if (GlobalRoundFormSubmitBtnIcon.classList.contains("fa-save")) {
+      logRound();
+    } else {
+      updateRound();
+    }
+   return;
+}
+//If here, at least one field is invalid: Display the errors
+//and allow user to fix them.
+GlobalRoundErrBox.classList.remove("hidden");
+document.title = "Error: Log Round";
+if (!notesValid) { 
+  GlobalRound.classList.remove("hidden");
+  GlobalRoundNotesErr.focus();
+  GlobalFirstFocusableLogRoundItem.set(GlobalRoundNotesErr);
+} else {
+  GlobalRoundNotesErr.classList.add("hidden");
+}
+if (!secondsValid) { 
+  GlobalRoundSecondsErr.classList.remove("hidden");
+  GlobalRoundSecondsErr.focus();
+  GlobalFirstFocusableLogRoundItem.set(GlobalRoundSecondsErr);
+} else {
+  GlobalRoundSecondsErr.classList.add("hidden");
+}
+if (!minutesValid) { 
+  GlobalRoundMinutesErr.classList.remove("hidden");
+  GlobalRoundMinutesErr.focus();
+  GlobalFrstFocusableLogRoundItem.set(GlobalRoundMinutesErr);
+} else {
+  GlobalRoundMinutesErr.classList.add("hidden");
+}
+if (!strokesValid) { 
+  GlobalRoundStrokesErr.classList.remove("hidden");
+  GlobalRoundStrokesErr.focus();
+  GlobalFirstFocusableLogRoundItem.set(GlobalRoundStrokesErr);
+} else {
+  GlobalRoundStrokesErr.classList.add("hidden");
+}
+if (!courseValid) { 
+    GlobalRoundCourseErr.classList.remove("hidden");
+    GlobalRoundCourseErr.focus();
+    GlobalFirstFocusableLogRoundItem.set(GlobalRoundCourseErr);
+} else {
+    GlobalRoundCourseErr.classList.add("hidden");
+}
+if (!dateValid) { 
+  GlobalRoundDateErr.classList.remove("hidden");
+  GlobalRoundDateErr.focus();
+  GlobalFirstFocusableLogRoundItem.set(GlobalRoundDateErr);
+} else {
+  GlobalRoundDateErr.classList.add("hidden");
+}
+});
+
+/*************************************************************************
+* @function keyDownRoundDialogFocused 
+* @desc 
+* When the user presses a key with an element in the Log Round 
+* dialog focused, we implement the accessible keyboard interface for
+* a modal dialog box. This means that "Escape" dismisses the dialog and
+* that it is impossible to tab outside of the dialog box.
+* @global GlobalFirstFocusableLogRoundItem: References the first focusable
+*         item in "Log Round" dialog. 
+* @global GlobalRoundsModeLogCancelBtn: The "Cancel" button (last focusable 
+*         item in "Log Round" dialog)
+*************************************************************************/
+function keyDownRoundDialogFocused(e) {
+if (e.code === "Escape") {
+  GlobalRoundsModeLogCancelBtn.click();
+  return;
+}
+if (e.code === "Tab" && document.activeElement == GlobalFirstFocusableLogRoundItem.get() &&
+   e.shiftKey) {
+    //shift focus to last focusable item in dialog
+    GlobalRoundsModeLogCancelBtn.focus();
+    e.preventDefault();
+    return;
+}
+if (e.code === "Tab" && document.activeElement == GlobalRoundsModeLogCancelBtn &&
+    !e.shiftKey) {
+    //shift focus to first focusable item in dialog
+    GlobalFirstFocusableLogRoundItem.get().focus();
+    e.preventDefault();
+    return;
+}
+}
