edce6d38caf421c821ffaca42ca3efa7610c1d08
Gerald Hendrix <geraldhendrix@Geralds-MacBook-Pro.local>
2026-04-28 23:05:55 -0700

    Added changeSeconds function to roundsMode.js #5

--- scripts/roundsMode.js
+++ scripts/roundsMode.js
@@ -18,3 +18,20 @@
     (GlobalRoundStrokes.valueAsNumber + GlobalRoundMinutes.valueAsNumber) + 
     ":" + GlobalRoundSeconds.value
 }
+
+/*************************************************************************
+* @function changeSeconds 
+* @Desc 
+* When the seconds field is updated, we need to ensure that the
+* seconds field of the round time is zero-padded. We also need to 
+* call updateSGS to update the speedgolf score based on the new seconds value.
+* @global roundStrokes: Form's strokes field
+* @global roundMinutes: Form's minutes field
+* @global roundSeconds: Form's seconds field
+*************************************************************************/
+function changeSeconds() {
+  if (GlobalRoundSeconds.value.length < 2) {
+    GlobalRoundSeconds.value = "0" + GlobalRoundSeconds.value;
+  }
+  updateSGS();
+}
